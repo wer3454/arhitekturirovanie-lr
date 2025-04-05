575644d16548c484696ed0d5ce90b53708a0fb0e
Константин Романов <const.romanov.dev@gmail.com>
2025-04-05 19:18:45 +0300

    laba 1 res

--- src/main/java/com/misis/apidemo/service/UserService.java
+++ src/main/java/com/misis/apidemo/service/UserService.java
@@ -6,8 +6,12 @@ import com.misis.apidemo.dto.UserCreateDTO;
 import com.misis.apidemo.dto.UserDTO;
 import com.misis.apidemo.dto.UserUpdateDTO;
 import com.misis.apidemo.dto.mapper.UserMapper;
+import com.misis.apidemo.service.cache.UserCacheService;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,16 +20,21 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class UserService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final UserCacheService userCacheService;
 
     @Autowired
     public UserService(
         UserRepository userRepository,
-        UserMapper userMapper
+        UserMapper userMapper,
+        UserCacheService userCacheService
     ) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.userCacheService = userCacheService;
     }
 
     public List<UserDTO> getAllUsers() {
@@ -33,8 +42,23 @@ public class UserService {
     }
 
     public UserDTO getUserById(UUID id) {
-        return userRepository.findById(id).map(userMapper::toDTO)
+        Optional<UserDTO> cachedUser = userCacheService.getFromCache(id);
+
+        // cache hit - нашел пользователя в кэше
+        //noinspection OptionalIsPresent
+        if (cachedUser.isPresent()) {
+            LOGGER.info("User cache hit");
+            return cachedUser.get();
+        }
+
+        LOGGER.info("User cache miss");
+        // cache miss - пользователя в кэше не оказалось
+        UserDTO userFromDB = userRepository.findById(id).map(userMapper::toDTO)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        // актуализирует кэш значением из БД
+        userCacheService.saveToCache(userFromDB);
+        return userFromDB;
     }
 
     public UserDTO createUser(UserCreateDTO userCreateDTO) {
@@ -55,11 +79,20 @@ public class UserService {
             user.setEmail(userUpdateDTO.email().get());
         }
 
-        return userMapper.toDTO(userRepository.save(user));
+        User savedUser = userRepository.save(user);
+
+        // после обновления - очищает данные из кэша
+        LOGGER.info("User cache evict on update");
+        userCacheService.removeFromCache(user.getId());
+
+        return userMapper.toDTO(savedUser);
     }
 
     public void deleteUser(UUID id) {
         userRepository.deleteById(id);
+        // после удаления - очищает данные из кэша
+        LOGGER.info("User cache evict on delete");
+        userCacheService.removeFromCache(id);
     }
 
 }
